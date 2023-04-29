ff701d97b9413f3ae099487b13da96f85d4ed11b
Loukoumpetit <loukoumpetit@gmail.com>
2023-04-29 21:38:53 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const visitBtn = document.getElementById('visit-btn');
 const clickSound = document.getElementById('click-sound');
 
@@ -11,3 +12,18 @@ setInterval(() => {
   visitBtn.style.animation = 'pop 0.3s ease-in-out 3s infinite';
 }, 3000);
 
+=======
+const visitBtn = document.getElementById('visit-btn');
+const clickSound = document.getElementById('click-sound');
+
+visitBtn.addEventListener('click', () => {
+  clickSound.play();
+});
+
+setInterval(() => {
+  visitBtn.style.animation = 'none';
+  void visitBtn.offsetWidth;
+  visitBtn.style.animation = 'pop 0.3s ease-in-out 3s infinite';
+}, 3000);
+
+>>>>>>> Stashed changes
