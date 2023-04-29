aa82222c1197db009c051a155df723441d3fe84c
Loukoumpetit <112101237+Loukoumpetit@users.noreply.github.com>
2023-04-29 21:51:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const visitBtn = document.getElementById('visit-btn');
 const clickSound = document.getElementById('click-sound');
 
@@ -11,19 +10,3 @@ setInterval(() => {
   void visitBtn.offsetWidth;
   visitBtn.style.animation = 'pop 0.3s ease-in-out 3s infinite';
 }, 3000);
-
-=======
-const visitBtn = document.getElementById('visit-btn');
-const clickSound = document.getElementById('click-sound');
-
-visitBtn.addEventListener('click', () => {
-  clickSound.play();
-});
-
-setInterval(() => {
-  visitBtn.style.animation = 'none';
-  void visitBtn.offsetWidth;
-  visitBtn.style.animation = 'pop 0.3s ease-in-out 3s infinite';
-}, 3000);
-
->>>>>>> Stashed changes
