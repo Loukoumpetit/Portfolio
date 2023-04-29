212fa4203b978f387fdb92d8412242f50972c60e
Loukoumpetit <112101237+Loukoumpetit@users.noreply.github.com>
2023-04-29 21:39:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const visitBtn = document.getElementById('visit-btn');
+const clickSound = document.getElementById('click-sound');
+
+visitBtn.addEventListener('click', () => {
+  clickSound.play();
+});
+
+setInterval(() => {
+  visitBtn.style.animation = 'none';
+  void visitBtn.offsetWidth;
+  visitBtn.style.animation = 'pop 0.3s ease-in-out 3s infinite';
+}, 3000);
+
